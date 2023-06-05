ac11471a2022d5fd1e2b69b077ce44b2515224e2
mrlokiie123 <133542179+mrlokiie123@users.noreply.github.com>
2023-06-04 20:16:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/oauth', (req, res) => {
+app.use('/oauth', (req, res) => {
     var username = req.params.username;
     
     res.send(username);
